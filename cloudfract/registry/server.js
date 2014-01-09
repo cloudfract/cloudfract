f8cdc4ed946f596519216347af9b3c9cd0f80f7c
John McKenzie <jmckind@gmail.com>
2014-01-09 02:05:59 -0600

    Process list and fetch messages

--- cloudfract/registry/server.js
+++ cloudfract/registry/server.js
@@ -1,5 +1,9 @@
+#!/usr/bin/env node
+
 var amqp = require("amqplib"),
     amqp_connection = "amqp://guest:guest@localhost:5672",
+    amqp_queue_list = "fractal.list",
+    amqp_queue_fetch = "fractal.fetch",
     amqp_queue_save = "fractal.save",
     amqp_queue_update = "fractal.update",
     couchdb_host = "http://localhost:5984",
@@ -16,15 +20,57 @@ function init_database() {
     });
 }
 
-// Process fractal save messages
-function save_fractal(message) {
+// Process list fractals messages
+function on_list_message(channel, message) {
     if (!message) {
-        log.warn("Unable to parse message");
+        log.warn("Unable to parse list fractals message");
+        return false;
+    }
+
+    var db = couchdb.use(couchdb_database);
+    db.view('fractal_design', 'fractal_list', function(err, body) {
+        if (err) {
+            console.warn("Unable to retrieve fractal list. %s", err);
+            return res.send(500, { error: err });
+        }
+
+        var fractal_list = body.rows.map(function(row) { return row.value });
+        var response = { fractals: fractal_list, total: body.total_rows, offset: body.offset };
+
+        channel.sendToQueue(message.properties.replyTo, new Buffer(JSON.stringify(response)), { correlationId: message.properties.correlationId });
+    });
+
+    return message;
+}
+
+// Process fetch fractal messages
+function on_fetch_message(channel, message) {
+    if (!message) {
+        log.warn("Unable to parse fetch fractal message");
+        return false;
+    }
+
+    var db = couchdb.use(couchdb_database);
+    db.get(message.content, function(err, body) {
+        if (err) {
+            console.warn("Unable to retrieve document. %s", err);
+            return res.send(500, { error: err });
+        }
+        channel.sendToQueue(message.properties.replyTo, new Buffer(JSON.stringify({ fractal: body })), { correlationId: message.properties.correlationId });
+    });
+
+    return message;
+}
+
+// Process fractal save messages
+function on_save_message(channel, message) {
+    if (!message) {
+        log.warn("Unable to parse save fractal message");
         return false;
     }
 
     var fractal = JSON.parse(message.content);
-    console.log(JSON.stringify(fractal));
+    console.log("Save Fractal: " + JSON.stringify(fractal));
 
     var db = couchdb.use(couchdb_database);
     db.get(fractal.id, function(err, body) {
@@ -47,14 +93,14 @@ function save_fractal(message) {
 }
 
 // Process fractal update messages
-function update_fractal(message) {    
+function on_update_message(channel, message) {    
     if (!message) {
-        log.warn("Unable to parse message");
+        log.warn("Unable to parse update fractal message");
         return false;
     }
 
     var fractal = JSON.parse(message.content);
-    console.log(JSON.stringify(fractal));
+    console.log("Update Fractal: " + JSON.stringify(fractal));
 
     var db = couchdb.use(couchdb_database);
     db.get(fractal.id, function(err, body) {
@@ -62,9 +108,11 @@ function update_fractal(message) {
             console.warn("Unable to retrieve document. " + err);
         } else {
             doc = body;
-            doc.name = fractal.name;
-            doc.notes = fractal.notes;
-            doc.status = fractal.status;
+            
+            for (var property in fractal) {
+                doc[property] = fractal[property];
+            }
+
             doc.updated = new Date();
 
             db.insert(doc, doc._id, function(err, body) {
@@ -86,14 +134,16 @@ function consumeQueue(channel, queue, callback) {
     channel.assertQueue(queue);
     console.log("Listening for requests on queue: " + queue);
     channel.consume(queue, function(message) {
-        channel.ack(callback(message));
+        channel.ack(callback(channel, message));
     });
 }
 
 // Handle processing once the channel has been created
 function onChannelCreated(channel) {
-    consumeQueue(channel, amqp_queue_save, save_fractal);
-    consumeQueue(channel, amqp_queue_update, update_fractal);
+    consumeQueue(channel, amqp_queue_list, on_list_message);
+    consumeQueue(channel, amqp_queue_fetch, on_fetch_message);
+    consumeQueue(channel, amqp_queue_save, on_save_message);
+    consumeQueue(channel, amqp_queue_update, on_update_message);
 }
 
 // Handle processing once the connection is established
